3581d09601315bd91789da88223945a3065324d8
nuttchai <nuttchai@outlook.com>
2022-08-06 03:41:56 +0700

    Enum Proto Struct in Golang

--- proto-go-course/main.go
+++ proto-go-course/main.go
@@ -15,6 +15,24 @@ func doSimple() *pb.Simple {
 	}
 }
 
+func doComplex() *pb.Complex {
+	return &pb.Complex{
+		OneDummy: &pb.Dummy{Id: 1, Name: "Hello1"},
+		MultipleDummies: []*pb.Dummy{
+			{Id: 2, Name: "Hello 2"},
+			{Id: 3, Name: "Hello 3"},
+		},
+	}
+}
+
+func doEnum() *pb.Enumerations {
+	return &pb.Enumerations{
+		EyeColor: 1, // or we can write => pb.EyeColor_EYE_COLOR_GREEN instead of 1,
+	}
+}
+
 func main() {
-	fmt.Println(doSimple())
+	// fmt.Println(doSimple())
+	// fmt.Println(doComplex())
+	fmt.Println(doEnum())
 }
