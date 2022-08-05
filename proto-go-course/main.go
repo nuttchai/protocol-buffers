fb33fe48473817f54bd772eda1acbf653ed55fbe
nuttchai <nuttchai@outlook.com>
2022-08-06 03:50:30 +0700

    Handling OneOfs in Golang

--- proto-go-course/main.go
+++ proto-go-course/main.go
@@ -31,8 +31,23 @@ func doEnum() *pb.Enumerations {
 	}
 }
 
+func doOneOf(message interface{}) {
+	switch x := message.(type) {
+	case *pb.Result_Id:
+		fmt.Println(message.(*pb.Result_Id).Id)
+	case *pb.Result_Message:
+		fmt.Println(message.(*pb.Result_Message).Message)
+	default:
+		fmt.Errorf("message has unexpected type: %v", x)
+	}
+}
+
 func main() {
 	// fmt.Println(doSimple())
 	// fmt.Println(doComplex())
-	fmt.Println(doEnum())
+	// fmt.Println(doEnum())
+	fmt.Println("This should be Id:")
+	doOneOf(&pb.Result_Id{Id: 42})
+	fmt.Println("This should be Message:")
+	doOneOf(&pb.Result_Message{Message: "a message"})
 }
